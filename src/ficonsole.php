1cf48a695127d94537ae1c194e8def217089760e
Tuncay Win Home <tunc270@gmail.com>
2020-05-17 15:10:42 +0300

    up

---
+++ src/ficonsole.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Simple helper to debug to the console
+*
+* @param $data object, array, string $data
+* @param $context string  Optional a description.
+*
+* @return string
+*/
+function debug_to_console($data, $context = 'Debug in Console') {
+
+// Buffering to solve problems frameworks, like header() in this and not a solid return.
+ob_start();
+
+$output  = 'console.info(\'' . $context . ':\');';
+$output .= 'console.log(' . json_encode($data) . ');';
+$output  = sprintf('<script>%s</script>', $output);
+
+echo $output;
+}
+
+?>
