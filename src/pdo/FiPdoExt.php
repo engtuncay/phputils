02142a5959b6a802161348c12821c8c6e66d1b7d
Tuncay_hp <tunc270@gmail.com>
2024-06-11 11:12:12 +0300

    up

--- src/pdo/FiPdoExt.php
+++ src/pdo/FiPdoExt.php
@@ -1,8 +1,5 @@
 <?php
-
-
-namespace Engtuncay\Phputils\pdo;
-
+namespace Engtuncay\Phputils\Pdo;
 
 use PDOException;
 use PDO;
@@ -13,46 +10,46 @@ use PDO;
  * 2024-06-10
  *
  */
-class FiPdoExt extends \PDO
+class FiPdoExt extends PDO
 {
-    public $debug = false;
+  public $debug = false;
 
-    public function __construct($host, $dbname, $username, $password, $charset = 'utf8')
-    {
-        try {
-            parent::__construct('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
-            $this->dbName = $dbname;
-            $this->query('SET CHARACTER SET ' . $charset);
-            $this->query('SET NAMES ' . $charset);
-            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        } catch (PDOException $e) {
-            $this->showError($e);
-        }
+  public function __construct($host, $dbname, $username, $password, $charset = 'utf8')
+  {
+    try {
+      parent::__construct('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+      $this->dbName = $dbname;
+      $this->query('SET CHARACTER SET ' . $charset);
+      $this->query('SET NAMES ' . $charset);
+      $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $e) {
+      $this->showError($e);
     }
+  }
 
-    public function from($tableName)
-    {
-        $this->sql = 'SELECT * FROM ' . $tableName;
-        $this->tableName = $tableName;
-        return $this;
-    }
+  public function from($tableName)
+  {
+    $this->sql = 'SELECT * FROM ' . $tableName;
+    $this->tableName = $tableName;
+    return $this;
+  }
 
-    public function select($columns)
-    {
-        $this->sql = str_replace(' * ', ' ' . $columns . ' ', $this->sql);
-        return $this;
-    }
+  public function select($columns)
+  {
+    $this->sql = str_replace(' * ', ' ' . $columns . ' ', $this->sql);
+    return $this;
+  }
 
-    /**
-     * Belirtilen tablonun auto_increment değerini ayarlar
-     *
-     * @param $tableName
-     * @return mixed
-     */
-    public function setAutoIncrement($tableName, $ai = 1)
-    {
-        return $this->query("ALTER TABLE `{$tableName}` AUTO_INCREMENT = {$ai}")->fetch();
-    }
+  /**
+   * Belirtilen tablonun auto_increment değerini ayarlar
+   *
+   * @param $tableName
+   * @return mixed
+   */
+  public function setAutoIncrement($tableName, $ai = 1)
+  {
+    return $this->query("ALTER TABLE `{$tableName}` AUTO_INCREMENT = {$ai}")->fetch();
+  }
 
 }
